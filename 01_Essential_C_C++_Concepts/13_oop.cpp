34fdfeb0f7a721020b474557d4e3ca712ed25fa0
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-07 21:42:01 +0530

    basic object orientation

---
+++ 01_Essential_C_C++_Concepts/13_oop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle {
+  private:
+    int length;
+    int breadth;
+  public: 
+    Rectangle(int l, int b){
+      length = l;
+      breadth = b;
+    }
+    int area(){
+      return length*breadth;
+    }
+    int perimeter(){
+      return 2*(length+breadth);
+    }
+};
+
+int main(){
+  Rectangle r(20, 5);
+  cout<<r.area()<<" "<<r.perimeter();
+}
